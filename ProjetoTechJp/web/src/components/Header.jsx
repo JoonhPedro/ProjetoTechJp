dd6fb4c19d424f10278b392f66d1cb949a0fdb5d
João Pedro <joao.veo77@gmail.com>
2023-10-19 22:02:36 -0300

    Configurando Ambiente

---
+++ ProjetoTechJp/web/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+const Header = () => {
+
+    return (
+        <>
+            <div className="header">
+                <div className="Logo">  
+                    <h1>TechJp</h1>
+                </div>
+
+                <div className="Barra-Pesquisa">
+
+                </div>
+
+                <div className="Painel-login">
+                    <h3>Login | Register</h3>
+                </div>
+
+                <div className="carrinho">
+
+                    <img src="" />
+                    <h3>Carrinho</h3>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
