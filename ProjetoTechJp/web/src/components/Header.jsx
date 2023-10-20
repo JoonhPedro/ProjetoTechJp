f23561165469c55974036d5bc6b2df56062951e9
João Pedro <joao.veo77@gmail.com>
2023-10-19 22:24:34 -0300

    Style: Modificação Header

--- ProjetoTechJp/web/src/components/Header.jsx
+++ ProjetoTechJp/web/src/components/Header.jsx
@@ -5,21 +5,24 @@ const Header = () => {
     return (
         <>
             <div className="header">
-                <div className="Logo">  
+                <div className="Logo">
                     <h1>TechJp</h1>
                 </div>
 
                 <div className="Barra-Pesquisa">
-
+                    <div className="Barra">
+                        <div className="Search">
+                            Search
+                        </div>
+                    </div>
                 </div>
-
                 <div className="Painel-login">
                     <h3>Login | Register</h3>
                 </div>
 
                 <div className="carrinho">
 
-                    <img src="" />
+                    <img src="./src/assets/image/carrinho.svg" />
                     <h3>Carrinho</h3>
 
                 </div>
