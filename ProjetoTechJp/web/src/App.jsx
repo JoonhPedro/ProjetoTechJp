519340983c13e36fc1a553c709f0b686508c12a7
João Pedro <joao.veo77@gmail.com>
2023-10-20 19:54:52 -0300

    style: Estruturando Sass

--- ProjetoTechJp/web/src/App.jsx
+++ ProjetoTechJp/web/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
+import Header from './assets/components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
